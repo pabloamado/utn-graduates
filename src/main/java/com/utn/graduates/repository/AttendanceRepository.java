a1b03ed336adacfaf6d69d4c2f44f970ba15a8b5
pabloamado <pablo.amado@despegar.com>
2024-06-09 18:24:50 -0300

    added attendance repository

--- src/main/java/com/utn/graduates/repository/AttendanceRepository.java
+++ src/main/java/com/utn/graduates/repository/AttendanceRepository.java
@@ -1,4 +1,9 @@
 package com.utn.graduates.repository;
 
-public class AttendanceRepository {
+import com.utn.graduates.model.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 }
